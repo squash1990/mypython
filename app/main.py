4a0b266f9c790240d12677f9f97492cc4c9206ff
squash1990 <jose.ignacio.cas@gmail.com>
2021-01-18 13:44:34 +0000

    App creation catalogo

--- app/main.py
+++ app/main.py
@@ -1,19 +1,34 @@
-from flask import jsonify, Flask
+from flask import jsonify, request, Flask
+from catalog import get_products, create_product
 
 app = Flask(__name__)
 
+@app.route('/product', methods=['GET', 'POST'])
+def list_all_products():
+	'''This vieu retrives all the products in te catalog'''
+	if request.method == 'GET':
+		response = get_products()
+		return jsonify(response)
+	if request.method == 'POST':
+		data = request.get_json()
+		create_product(
+			data['sku'],
+			data['title'],
+			data['long_description'],
+			data['price_euro'])
+		return jsonify({"status": "ok"})
 @app.route('/hello')
 def hello_world():
-    message = "Hola mundo, quiero presentar mi primer programa desde Google Cloud Run"
-    response = {
-            "messaje": message,
-            "length": len(message)
-    }
-    return jsonify(response)
+	message = "Hola Mundo, soy Pyton! Ahora con CloudBuild y hablando JSON."
+	response = {
+			"messaje": message,
+			"length": len(message)
+	}
+	return jsonify(response)
 
 @app.route('/bye')
 def bye_world():
     return ("Adios mundo cruel")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+	app.run(debug=True, host='0.0.0.0', port=5000)
