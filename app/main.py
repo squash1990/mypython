826de85b286d9a7b931fb7ec8c6a520ab43dc52d
squash1990 <squash@vm-tools.europe-west1-b.c.proyecto-prueba-assoo.internal>
2021-01-18 12:08:55 +0000

    Commit inicial

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import jsonify, Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    message = "Hola mundo, quiero presentar mi primer programa"
+    response = {
+            "messaje": message,
+            "length": len(message)
+    }
+    return jsonify(response)
+
+@app.route('/bye')
+def bye_world():
+    return ("Adios mundo cruel")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
